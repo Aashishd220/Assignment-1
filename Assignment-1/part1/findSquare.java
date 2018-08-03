f73daf851608304647e09d5aba0858c1b7754bf5
Aashishd220 <41561579+Aashishd220@users.noreply.github.com>
2018-08-04 00:08:55 +0530

    Add files via upload

---
+++ Assignment-1/part1/findSquare.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class findSquare {
+	// returns square of a number
+	static int square(int output) {
+		return output * output;
+	}
+
+	public static void main(String a[]) {
+		Scanner get = new Scanner(System.in);
+		int input;
+		input = get.nextInt();
+		get.close();
+
+		System.out.print(square(input));// printing square using square()
+	}
+}
